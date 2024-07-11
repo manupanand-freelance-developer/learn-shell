7635509376bfa63d24dddfd931fb6c37ede4c3f4
manupanand <manupanandindianinvestor@gmail.com>
2024-07-11 21:59:36 +0530

    new commands if else

--- 01-demo.bash
+++ 01-demo.bash
@@ -1,7 +1,29 @@
+
+function check(){
+if [$? -eq 0]; then
+  echo SUCCESS
+else
+  echo FAILED
+fi
+}
 bash bash.sh
+check
 
-dnf module disable nginx -y
-dnf module enable nginx:1.24 -y
-dnf install nginx -y
+# dnf module disable nginx -y
+# dnf module enable nginx:1.24 -y
+# dnf install nginx -y  &>/dev/null
 
-systemctl enable nginx
+ sudo systemctl enable nginx 
+check
+
+RED='\033[0;32m'
+GREEN='\033[0;31m'
+NC='\033[0m'
+printSuccess(){
+  echo -e "${GREEN}$1${NC}"
+}
+printFail(){
+  echo  -e "${RED}$1${NC}"
+}
+printSuccess SUCCESS
+printFail FAILED
