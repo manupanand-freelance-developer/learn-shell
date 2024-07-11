9c015e89165f77350b9f96f42e0a63404723a7ff
manupanand <manupanandindianinvestor@gmail.com>
2024-07-12 00:18:58 +0530

    new learning color and if

--- 01-demo.bash
+++ 01-demo.bash
@@ -13,8 +13,8 @@ check
 # dnf module enable nginx:1.24 -y
 # dnf install nginx -y  &>/dev/null
 
- sudo systemctl enable nginx 
-check
+#  sudo systemctl enable nginx 
+# check
 
 RED='\033[0;32m'
 GREEN='\033[0;31m'
@@ -24,6 +24,22 @@ printSuccess(){
 }
 printFail(){
   echo  -e "${RED}$1${NC}"
+  echo 
+  echo  -e  "\e[33mRefer the log file for more information \e[0m"
+  exit $1 #on failure exit
 }
 printSuccess SUCCESS
 printFail FAILED
+
+#-e to enable 
+#\e to enable color
+#[COLORm-31m-36m
+#0m-end color-reset otherwise it will continue
+echo  -e  "\e[31mTEST\e[0m"
+echo  -e  "\e[31m${1}\e[0m"
+echo  -e  "\e[31mRED\e[0m"
+echo  -e  "\e[32mGREEN\e[0m"
+echo  -e  "\e[33mYELLOW\e[0m"
+echo  -e  "\e[34mBLUE\e[0m"
+echo  -e  "\e[35mMAGENTA\e[0m"
+echo  -e  "\e[36mCYAN\e[0m"
