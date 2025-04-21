a96aeec223f33491d2a8379ccd1b600457dda7b5
manupanand <manupanandindianinvestor@gmail.com>
2025-04-21 23:16:29 +0530

    updaet script revice

--- revice.sh
+++ revice.sh
@@ -8,4 +8,18 @@ echo 'hello manu'
 #
 #append>>
 #stderr 2>file
-#/dev/null - nullify output 
+#/dev/null - nullify output
+#
+echo test
+echo  -e 'b\bc'
+
+
+printf "%f\n" 255
+printf "%s\n" 255
+printf "%d\n" 255
+printf "%.1f\n" 256
+
+
+for i in $(seq 1 10);do printf "%04d\t" "$i"; done
+
+exit 220
